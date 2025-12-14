ad33f8bda2c952b4597a4dc0ff18df566b2f87ef
SashaEEone <eeoneguy3984@gmail.com>
2025-12-14 18:38:39 +0700

    пукнты до 15

--- sort.c
+++ sort.c
@@ -1,4 +1,4 @@
-
+#include <stdio.h>
 // Принимает: массив и его размер
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n-1; i++) {
@@ -10,4 +10,28 @@ void bubbleSort(int arr[], int n) {
             }
         }
     }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    // Объявляем массив из нескольких чисел
+    int a[] = {4, 2, 0, 9, 5, 1, 7, 3, 8};
+    int n = sizeof(a) / sizeof(a[0]); // Вычисляем размер массива
+    
+    printf("Исходный массив: ");
+    printArray(a, n);
+    
+    // Вызываем функцию сортировки
+    bubbleSort(a, n);
+    
+    printf("Отсортированный массив: ");
+    printArray(a, n);
+    
+    return 0;
 }
